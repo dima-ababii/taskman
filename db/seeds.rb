7e84bd7785d6ccc958bb17c3ec39c9fff8083b06
Dmytro Ababii <ababiids@gmail.com>
2018-12-17 11:31:49 +0200

    Add Settings to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@ User.create!(email: 'vader@mail.com', password: '123123', password_confirmation:
 User.create!(email: 'obi@mail.com', password: '123123', password_confirmation: '123123', role: 'student', first_name: 'Kenobi', last_name: 'ObiVan') unless Rails.env.production?
 User.create!(email: 'mol@mail.com', password: '123123', password_confirmation: '123123', role: 'student', first_name: 'Mol', last_name: 'Dart') unless Rails.env.production?
 User.create!(email: 'windu@mail.com', password: '123123', password_confirmation: '123123', role: 'student', first_name: 'Windu', last_name: 'Magister') unless Rails.env.production?
+
+TaskSetting.get_task_settings
